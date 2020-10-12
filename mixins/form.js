bfe23d2068036bc17aca3a84f77138eb0da0a86f
Zach Winter <contact@zachwinter.com>
2020-10-11 20:24:03 -0500

    1.0.4

--- mixins/form.js
+++ mixins/form.js
@@ -1,11 +1,19 @@
 import TextInput from '../components/forms/TextInput'
 import TextArea from '../components/forms/TextArea'
 import SubmitButton from '../components/forms/SubmitButton'
+import RadioGroup from '../components/forms/RadioGroup'
+import RangeInput from '../components/forms/RangeInput'
+import SelectDropdown from '../components/forms/SelectDropdown'
+import Toggle from '../components/forms/Toggle'
 
 export default {
   components: {
     TextInput,
     TextArea,
-    SubmitButton
+    SubmitButton,
+    RadioGroup,
+    RangeInput,
+    SelectDropdown,
+    Toggle
   }
 }
