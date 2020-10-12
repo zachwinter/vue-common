561548847ef1eebc17db42850dc4f8fd1df06980
Zach Winter <contact@zachwinter.com>
2020-10-11 23:34:32 -0500

    1.0.14

--- util/store.js
+++ util/store.js
@@ -1,89 +1,80 @@
-export function buildModule ({ state, actions }) {
+function _buildMutationString(list) { // eslint-disable-line
+  return list.reduce((acc, val, i) => {  // eslint-disable-line
+    return acc + val.toUpperCase() + (i === list.length - 1 ? '' : '_');
+  }, 'SET_');
+}
+
+function _exposeMutations(state) { // eslint-disable-line
+  return Object.keys(state)
+    .map((key) => ({ key, list: key.split(/(?=[A-Z])/) }))
+    .reduce((acc, { key, list }) => {
+      const mutation = _buildMutationString(list);
+      acc[mutation] = key;
+      return acc;
+    }, {});
+}
+
+function _extract(prop) { // eslint-disable-line
+  const split = prop.split('/');
+  const [module, key] = split.length == 2 ? split : [null, split[1]];  // eslint-disable-line
+  const list = key.split(/(?=[A-Z])/);
+  const mutation = _buildMutationString(list);
+  return { mutation, module, key };
+}
+
+function _dualBind(prop, localName) { // eslint-disable-line
+  const { mutation, module, key } = _extract(prop);
+  return {
+    [localName || key]: {
+      get() {
+        return module ? this.$store.state[module][key] : this.$store.state[key];
+      },
+
+      set(val) {
+        return this.$store.commit(`${module}/${mutation}`, val);
+      },
+    },
+  };
+}
+
+function _bind(prop, localName) { // eslint-disable-line
+  const { module, key } = _extract(prop);
+  return {
+    [localName || key]() {
+      return module ? this.$store.state[module][key] : this.$store.state[key];
+    },
+  };
+}
+
+export function composeMutations(state) {
+  const mutations = _exposeMutations(state);
+  return Object.keys(mutations).reduce((acc, mutation) => {
+    acc[mutation] = function (state, val) {  // eslint-disable-line
+      state[mutations[mutation]] = val;
+    };
+    return acc;
+  }, {});
+}
+
+export function buildModule({ state, actions }) {
   return {
     namespaced: true,
     state,
-    mutations: _composeMutations(state),
-    actions
-  }
+    mutations: composeMutations(state),
+    actions,
+  };
 }
 
-export function bind (prop, localName = null) {
+export function bind(prop, localName = null) {
   if (Array.isArray(prop)) {
-    return prop.reduce((acc, prop) => {
-      acc = { ...acc, ..._bind(prop) }
-      return acc
-    }, {})
-  } else {
-    return _bind(prop, localName)
+    return prop.reduce((acc, p) => ({ ...acc, ..._bind(p) }), {});
   }
+  return _bind(prop, localName);
 }
 
-export function dualBind (prop, localName = null) {
+export function dualBind(prop, localName = null) {
   if (Array.isArray(prop)) {
-    return prop.reduce((acc, prop) => {
-      acc = { ...acc, ..._dualBind(prop) }
-      return acc
-    }, {})
-  } else {
-    return _dualBind(prop, localName)
+    return prop.reduce((acc, p) => ({ ...acc, ..._dualBind(p) }), {});
   }
-}
-
-function _exposeMutations (state) {
-  return Object.keys(state).map(key => {
-    return { key, list: key.split(/(?=[A-Z])/) }
-  }).reduce((acc, { key, list }) => {
-    const mutation = _buildMutationString(list)
-    acc[mutation] = key
-    return acc
-  }, {})
-}
-
-function _composeMutations (state) {
-  const mutations = _exposeMutations(state)
-  return Object.keys(mutations).reduce((acc, mutation) => {
-    acc[mutation] = function (state, val) {
-      state[mutations[mutation]] = val
-    }
-    return acc
-  }, {})
-}
-
-function _buildMutationString (list) {
-  return list.reduce((acc, val, i) => {
-    acc += val.toUpperCase() + (i === list.length - 1 ? '' : '_')
-    return acc
-  }, 'SET_')
-}
-
-function _extract (prop) {
-  const split = prop.split('/')
-  const [module, key] = split.length == 2 ? split : [null, split[1]]
-  const list = key.split(/(?=[A-Z])/)
-  const mutation = _buildMutationString(list)
-  return { mutation, module, key }
-}
-
-function _dualBind (prop, localName) {
-  const { mutation, module, key } = _extract(prop)
-  return { 
-    [localName || key]: {
-      get () {
-        return module ? this.$store.state[module][key] : this.$store.state[key]
-      },
-
-      set (val) {
-        return this.$store.commit(`${module}/${mutation}`, val)
-      }
-    }
-  }
-}
-
-function _bind (prop, localName) {
-  const { module, key } = _extract(prop)
-  return {
-    [localName || key] () {
-      return module ? this.$store.state[module][key] : this.$store.state[key]
-    }
-  }
+  return _dualBind(prop, localName);
 }
